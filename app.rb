5f37e6dc575710268ca020343b3050b11629050d
jonathankolsrud <jonathan.kolsrud@elev.ga.ntig.se>
2024-03-04 12:31:59 +0100

    2024-03-04
    mitt

--- app.rb
+++ app.rb
@@ -1,8 +1,30 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
 get('/') do
+    
+    slim(:home)
+end
+
+get('/bodymaker') do
     slim(:bodymaker)
+end
+
+post('/bodymaker') do
+    height = params[:height]
+    weight = params[:weight]
+    p height
+    p weight
+    db = SQLite3::Database.new("db/db.db")
+    height_id = db.execute("SELECT id FROM heights WHERE height = ?", height).first.first
+    weight_id = db.execute("SELECT id FROM weights WHERE weight = ?", weight).first.first
+    size = db.execute("SELECT size FROM heightweightsize WHERE height = ? AND weight = ?", height_id, weight_id).first.first
+    slim(:"sizedone",locals:{size:size})
+end
+
+get('/sizedone') do
+slim(:sizedone)
 end
