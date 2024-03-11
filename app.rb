a72338a956ed822e5e4f1fe41e3a575c007a47b9
jonathankolsrud <jonathan.kolsrud@elev.ga.ntig.se>
2024-03-11 10:19:17 +0100

    2024-03-04

--- app.rb
+++ app.rb
@@ -4,27 +4,103 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
+enable :sessions
+
 get('/') do
-    
-    slim(:home)
+    if nil == session[:id]
+        redirect('/register')
+    else
+        slim(:bodymaker)
+    end
+end
+
+get('/register') do
+    slim(:register)
 end
 
 get('/bodymaker') do
+    if nil == session[:id]
+        redirect('/showlogin')
+    end
     slim(:bodymaker)
 end
 
 post('/bodymaker') do
+    id = session[:id]
     height = params[:height]
     weight = params[:weight]
-    p height
-    p weight
     db = SQLite3::Database.new("db/db.db")
     height_id = db.execute("SELECT id FROM heights WHERE height = ?", height).first.first
     weight_id = db.execute("SELECT id FROM weights WHERE weight = ?", weight).first.first
-    size = db.execute("SELECT size FROM heightweightsize WHERE height = ? AND weight = ?", height_id, weight_id).first.first
-    slim(:"sizedone",locals:{size:size})
+    session[:size] = db.execute("SELECT size FROM heightweightsize WHERE height = ? AND weight = ?", height_id, weight_id).first.first
+    redirect('/sizedone')
 end
 
 get('/sizedone') do
-slim(:sizedone)
+    slim(:sizedone)
+end
+
+post('/sizedone') do
+    user_id = session[:id].to_i
+    topsize = session[:size]
+    bottomsize = "test"
+    db = SQLite3::Database.new("db/db.db")
+    db.execute('INSERT INTO bodies (user_id,topsize,bottomsize) VALUES (?,?,?)',user_id,topsize,bottomsize)
+    redirect('/bodies')
+end
+
+get('/bodies') do
+    id = session[:id].to_i
+    db = SQLite3::Database.new('db/db.db')
+    result = db.execute("SELECT * FROM bodies WHERE user_id = ?",id)
+    p "Alla bodies från result lol #{result}"
+    slim(:"/bodies",locals:{bodies:result})
+end  
+
+
+
+get('/showlogin') do
+    slim(:login)
+end  
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirmed = params[:password_confirmed]
+  
+    if (password == password_confirmed)
+      #lägg till användare
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/db.db')
+      db.execute('INSERT INTO users (username,pwdigest) VALUES (?,?)',username,password_digest)
+      redirect('/bodymaker')
+  
+    else
+      #felhantering
+      "lösenorden matchade inte"
+    end
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/db.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        session[:username] = username
+        redirect('/bodymaker')
+        
+    else
+        "FEL LÖSEN HAHA!"
+    end
+end
+
+get('/logout') do
+    session[:id] = nil
+    session[:username] = nil
+    redirect('/register')
 end
