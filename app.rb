53f2e33bed517cb7c14a73918f05a78b79cca6f6
jonathankolsrud <jonathan.kolsrud@elev.ga.ntig.se>
2024-04-11 21:02:02 +0200

    2024-04-11

--- app.rb
+++ app.rb
@@ -8,45 +8,74 @@ require_relative 'model.rb'
 enable :sessions
 
 get('/') do
-    no_login()
+    user()
 end
 
 get('/register') do
     slim(:register)
 end
 
-get('/bodymaker') do
-    no_login()
-    slim(:bodymaker)
+get('/bodies/new') do
+    user()
+    slim(:new)
 end
 
-post('/bodymaker') do
+post('/bodies/new') do
     height = params[:height]
     weight = params[:weight]
     get_size(height, weight)
-    redirect('/sizedone')
+    redirect('/bodies/create')
 end
 
-get('/sizedone') do
-    slim(:sizedone)
+get('/bodies/create') do
+    user()
+    slim(:create)
 end
 
-post('/sizedone') do
+post('/bodies/create') do
     bodyname = params[:bodyname]
-    save_size(bodyname)
-    redirect('/bodies')
+    save_body(bodyname)
+    redirect('/bodies/index')
 end
 
-get('/bodies') do
-    no_login()
+get('/bodies/index') do
+    user()
     result = all_bodies_for_user()
-    slim(:"/bodies",locals:{bodies:result})
+    slim(:"/index",locals:{bodies:result})
 end  
+
+get('/bodies/index_admin') do
+    user()
+    admin()
+    result = all_bodies_for_everyone()
+    slim(:"/index_admin",locals:{bodies:result})
+end  
+
+get('/bodies/:id/edit') do
+    id = params[:id].to_i
+    edit_body(id)
+end
+
+post('/bodies/:id/update') do
+    if "admin"==session[:authority]
+        new_id =  params[:admin_id].to_i
+        new_user_id = params[:user_id].to_i
+    else
+        new_id =  params[:id].to_i
+        new_user_id = session[:id].to_i
+    end
+    id = params[:id].to_i
+    bodyname = params[:bodyname]
+    size = params[:size]
+    height = params[:height]
+    weight = params[:weight]
+    update_body(id, bodyname, size, height, weight, new_id, new_user_id)
+end
 
 post('/bodies/:id/delete') do
     body_id = params[:id].to_i
     delete_body(body_id)
-    redirect('/bodies')
+    redirect('/bodies/index')
 end
 
 get('/showlogin') do
--- model.rb
+++ model.rb
@@ -1,16 +1,19 @@
 #User
-def no_login()
+def user()
     if nil == session[:id]
         redirect('/showlogin')
     end
+end
+
+def admin()
 end
 
 def register(username, password, password_confirmed)
     if (password == password_confirmed)
         password_digest = BCrypt::Password.create(password)
         db = SQLite3::Database.new('db/db.db')
-        db.execute('INSERT INTO users (username,pwdigest) VALUES (?,?)',username,password_digest)
-        redirect('/bodymaker')
+        db.execute('INSERT INTO users (username,pwdigest,authority) VALUES (?,?,?)',username,password_digest,"user")
+        redirect('/bodies/new')
     else
         "lösenorden matchade inte"
     end
@@ -21,15 +24,15 @@ def login(username, password)
     db.results_as_hash = true
     result = db.execute("SELECT * FROM users WHERE username = ?",username).first
     pwdigest = result["pwdigest"]
-    id = result["id"]
     if BCrypt::Password.new(pwdigest) == password
-        session[:id] = id
-        session[:username] = username
-        redirect('/bodymaker')
-        
+        session[:id] = result["id"]
+        session[:authority] = result["authority"]
+        session[:username] = result["username"]
+        redirect('/bodies/new')
     else
         "FEL LÖSEN HAHA!"
     end
+    p session[:authority]
 end
 
 
@@ -47,11 +50,24 @@ end
 def save_body(bodyname)
     user_id = session[:id].to_i
     topsize = session[:size]
-    bottomsize = session[:size]
     height = session[:height]
     weight = session[:weight] 
     db = SQLite3::Database.new("db/db.db")
-    db.execute('INSERT INTO bodies (bodyname,user_id,topsize,bottomsize,height,weight) VALUES (?,?,?,?,?,?)',bodyname,user_id,topsize,bottomsize,height,weight)
+    db.execute('INSERT INTO bodies (bodyname,user_id,size,height,weight) VALUES (?,?,?,?,?)',bodyname,user_id,topsize,height,weight)
+end
+
+def edit_body(id)
+    db = SQLite3::Database.new("db/db.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM bodies WHERE id = ?",id)
+    p result
+    slim(:"/edit",locals:{bodies:result})
+end
+
+def update_body(id, bodyname, size, height, weight, new_id, new_user_id)
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("UPDATE bodies SET bodyname=?,height=?,weight=?,size=?,user_id=?,id=? WHERE id = ?", bodyname,height,weight,size,new_user_id,new_id,id)
+    redirect('/bodies/index')
 end
 
 def delete_body(body_id)
@@ -64,5 +80,11 @@ def all_bodies_for_user()
     db = SQLite3::Database.new('db/db.db')
     db.results_as_hash = true
     db.execute("SELECT * FROM bodies WHERE user_id = ?",id)
+end
+
+def all_bodies_for_everyone()
+    db = SQLite3::Database.new('db/db.db')
+    db.results_as_hash = true
+    db.execute("SELECT * FROM bodies")
 end
 
