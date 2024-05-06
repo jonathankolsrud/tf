ae552f64720e24de4eaf233bf1f9aefdd4dea7bc
jonathankolsrud <jonathan.kolsrud@elev.ga.ntig.se>
2024-05-06 18:26:40 +0200

    2024-05-05

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
-require_relative 'model.rb'
+require_relative 'model/model.rb'
 require 'sinatra/flash'
+
+include Model
 
 
 enable :sessions
