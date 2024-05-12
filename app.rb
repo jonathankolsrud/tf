e249163bf353771458ddb0028a4ede382faeb377
jonathankolsrud <jonathan.kolsrud@elev.ga.ntig.se>
2024-05-12 22:14:56 +0200

    yardoc 2

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ before('/admin/*') do
     end
 end
 
-#
+
 # Displays the Landing Page
 #
 get('/') do
@@ -54,6 +54,10 @@ end
 
 # Calculates the new body
 #
+# @param [Integer] :height, the body's height
+# @param [Integer] :weight, the body's weight
+#
+# @see Model#get_size
 post('/user/bodies/new') do
     height = params[:height]
     weight = params[:weight]
@@ -61,22 +65,26 @@ post('/user/bodies/new') do
     redirect('/user/bodies')
 end
 
-#Displays page for saving the new body
+# Displays page for saving the new body
 #
 get('/user/bodies') do
     slim(:"/bodies/create")
 end
 
-#Saves the new body
+# Saves the new body and redirects to '/user/bodies/index'
 #
+# @param [String] :bodyname, the body's name
+#
+# @see Model#save_body
 post('/user/bodies') do
     bodyname = params[:bodyname]
     save_body(bodyname)
     redirect('/user/bodies/index')
 end
 
-#Displays page for showing all your bodies
+# Displays page for showing all your bodies
 #
+# @see Model#all_bodies_for_user
 get('/user/bodies/index') do
     result = all_bodies_for_user()
     slim(:"/bodies/index",locals:{bodies:result, bodiesadmin:nil})
@@ -84,20 +92,32 @@ end
 
 #Displays page for showing everyones bodies
 #
+# @see Model#all_bodies_for_everyone
 get('/admin/bodies/index') do
     resultadmin = all_bodies_for_everyone()
     slim(:"/bodies/index",locals:{bodiesadmin:resultadmin})
 end  
 
-#Displays page for editing body
+# Displays page for editing body
 #
+# @param [Integer] :id, the body's id
+#
+# @see Model#edit_body
 get('/user/bodies/:id/edit') do
     id = params[:id].to_i
     edit_body(id)
 end
 
-#Updates body
+# Updates body and redirects to '/user/bodies/index'
 #
+# @param [Integer] :user_id, the users id
+# @param [Integer] :id, the body's id
+# @param [String] :bodyname, the body's name
+# @param [String] :size, the body's size
+# @param [Integer] :height, the body's height
+# @param [Integer] :weight, the body's weight
+#
+# @see Model#update_body
 post('/user/bodies/:id/update') do
     if "admin"==session[:authority]
         new_user_id = params[:user_id].to_i
@@ -113,22 +133,29 @@ post('/user/bodies/:id/update') do
     redirect('/user/bodies/index')
 end
 
-#Deletes body
+# Deletes body and redirects to '/user/bodies/index'
 #
+# @param [Integer] :id, the body's id
+#
+# @see Model#delete_body
 post('/user/bodies/:id/delete') do
     body_id = params[:id].to_i
     delete_body(body_id)
     redirect('/user/bodies/index')
 end
 
-#Displays page for login
+# Displays page for login
 #
 get('/showlogin') do
     slim(:login)
 end  
 
-#Logs in user
+# Attempts login and redirects to '/user/bodies/new'
 #
+# @param [String :username, the users username
+# @param [String] :password, the users password
+#
+# @see Model#login
 post('/login') do
     username = params[:username]
     password = params[:password]
@@ -147,14 +174,19 @@ post('/login') do
     end
 end
 
-#Displays page for register
+# Displays page for register
 #
 get('/register') do
     slim(:register)
 end
 
-#Registers user
+#Attempts registers and redirects to '/showlogin'
 #
+# @param [String :username, the users username
+# @param [String] :password, the users password
+# @param [String] :password_confirmed, the users password
+#
+# @see Model#register
 post('/register') do
     username = params[:username]
     password = params[:password]
@@ -184,7 +216,7 @@ post('/register') do
     end
 end
 
-#Logouts user
+#Logouts user, clears the session and redirects to '/showlogin'
 #
 get('/logout') do
     session.clear
--- model/model.rb
+++ model/model.rb
@@ -1,10 +1,18 @@
 module Model
 
-    #User
+    # Returns database items from specified table
+    #
+    # @param [String] path Path to database
     def connect_to_db(path)
         db = SQLite3::Database.new(path)
     end
 
+    # Checks if user owns body
+    #
+    # @param [Integer] body_id The body's id
+    #
+    # @return [Bolean] true The user owns the body
+    # @return [Bolean] false The user does not own the body
     def owns_body?(body_id)
         db = connect_to_db('db/db.db')
         user_id = db.execute("SELECT user_id FROM bodies WHERE id = ?", body_id).first.first
@@ -15,34 +23,68 @@ module Model
         end
     end
 
-
-
-    #register
+    # Registers user
+    # 
+    # @param [String] username The users username
+    # @param [String] password The users password
     def register(username, password)
         pwdigest = BCrypt::Password.create(password)
         db = connect_to_db('db/db.db')
         db.execute('INSERT INTO users (username,pwdigest,authority) VALUES (?,?,?)',username,pwdigest,"user")
     end
 
-    #validering
+    # Returns whether username or password field is empty
+    #
+    # @param [String] username The users username
+    # @param [String] password The users password
+    #
+    # @return [Bolean] true One of the fields are empty
+    # @return [Bolean] false None of the field are empty
     def field_empty?(username, password)
         p username && password
         return username && password == ""
     end
 
+
+    # Returns whether user exists
+    #
+    # @param [String] username The users username
+    #
+    # @return [Bolean] true The user exists
+    # @return [Bolean] false The user does not exist
     def user_exist?(username)
         db = connect_to_db('db/db.db')
         return db.execute("SELECT * FROM users WHERE username = ?", username).first != nil
     end
 
+    # Returns whether passwords match
+    #
+    # @param [String] password The users password
+    #
+    # @return [Bolean] true The passwords match
+    # @return [Bolean] false The password do not match
     def comparepassword(password, password_confirmed)
         return (password == password_confirmed)
     end
 
+    # Returns whether length of either username or password is too short
+    #
+    # @param [String] password The users password
+    # @param [String] username The users username
+    #
+    # @return [Bolean] true The username & password are not too short
+    # @return [Bolean] false The username or password are too short
     def lengthvalidation(username, password)
         return username.length >= 4 && password.length >= 6
     end
 
+    # Returns whether password is correct
+    #
+    # @param [String] password The users password
+    # @param [String] username The users username
+    #
+    # @return [Bolean] true The password is correct
+    # @return [Bolean] false The password is not correct
     def passwordvalidation(username, password)
         db = connect_to_db('db/db.db')
         db.results_as_hash = true
@@ -51,6 +93,9 @@ module Model
         return BCrypt::Password.new(pwdigest) == password
     end
 
+    # Logins user by updating the sessions
+    #
+    # @param [String] username The users username
     def login(username)
         db = connect_to_db('db/db.db')
         db.results_as_hash = true
@@ -60,8 +105,10 @@ module Model
         session[:username] = result["username"]
     end
 
-
-    #Size
+    # Gets size from table and updates the sessions
+    #
+    # @param [Integer] height The body's height
+    # @param [Integer] weight The body's weight
     def get_size(height, weight)
         db = connect_to_db('db/db.db')
         height_id = db.execute("SELECT id FROM heights WHERE height = ?", height).first.first
@@ -71,46 +118,65 @@ module Model
         session[:weight] = weight
     end
 
-    #Bodies
+    # Saves body with the bodyname, user_id, size, height and weight
+    #
+    # @param [String] bodyname The body's name
     def save_body(bodyname)
         db = connect_to_db('db/db.db')
         user_id = session[:id].to_i
-        topsize = session[:size]
+        size = session[:size]
         height = session[:height]
         weight = session[:weight] 
-        db.execute('INSERT INTO bodies (bodyname,user_id,size,height,weight) VALUES (?,?,?,?,?)',bodyname,user_id,topsize,height,weight)
+        db.execute('INSERT INTO bodies (bodyname,user_id,size,height,weight) VALUES (?,?,?,?,?)',bodyname,user_id,size,height,weight)
     end
 
+    # Gets info about specific body
+    #
+    # @param [Integer] id The body's id
     def edit_body(id)
         db = connect_to_db('db/db.db')
         db.results_as_hash = true
         result = db.execute("SELECT * FROM bodies WHERE id = ?",id)
-        p result
         slim(:"/edit",locals:{bodies:result})
     end
 
+    # Updates the body 
+    #
+    # @param [Integer] new_user_id, New users id
+    # @param [Integer] body_id New body's id
+    # @param [String] bodyname New body's name
+    # @param [String] :size New body's size
+    # @param [Integer] :height New body's height
+    # @param [Integer] :weight New body's weight
     def update_body(body_id, bodyname, size, height, weight, new_user_id)
         db = connect_to_db('db/db.db')
         db.execute("UPDATE bodies SET bodyname=?,height=?,weight=?,size=?,user_id=? WHERE id = ?", bodyname,height,weight,size,new_user_id,body_id)
     end
 
+    # Deletes body
+    #
+    # @param [Integer] body_id The body's id
     def delete_body(body_id)
         db = connect_to_db('db/db.db')
         user_id = db.execute("SELECT user_id FROM bodies WHERE id = ?",body_id).first.first
         bodies = db.execute("DELETE FROM bodies WHERE id = ?",body_id)
     end
 
+    # Returns all bodies for user
+    #
     def all_bodies_for_user()
         db = connect_to_db('db/db.db')
         id = session[:id].to_i
         db.results_as_hash = true
-        db.execute("SELECT * FROM bodies WHERE user_id = ?",id)
+        return db.execute("SELECT * FROM bodies WHERE user_id = ?",id)
     end
 
+    # Returns all bodies for everyone
+    #
     def all_bodies_for_everyone()
         db = connect_to_db('db/db.db')
         db.results_as_hash = true
-        db.execute("SELECT * FROM bodies")
+        return db.execute("SELECT * FROM bodies")
     end
 
 end
