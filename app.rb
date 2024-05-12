a258b7242d2fc84b0ba02fe0ad70fe95cff0b828
jonathankolsrud <jonathan.kolsrud@elev.ga.ntig.se>
2024-05-12 20:35:47 +0200

    yardoc 1

--- app.rb
+++ app.rb
@@ -11,6 +11,8 @@ include Model
 
 enable :sessions
 
+# Before block
+#
 before('/user/*') do
     if nil == session[:id]
         redirect('/showlogin')
@@ -36,14 +38,22 @@ before('/admin/*') do
     end
 end
 
+#
+# Displays the Landing Page
+#
 get('/') do
-    redirect('/showlogin')
+    redirect('/user/bodies/index')
 end
 
+
+# Displays a page for calculating a new body
+#
 get('/user/bodies/new') do
     slim(:"/bodies/new")
 end
 
+# Calculates the new body
+#
 post('/user/bodies/new') do
     height = params[:height]
     weight = params[:weight]
@@ -51,31 +61,43 @@ post('/user/bodies/new') do
     redirect('/user/bodies')
 end
 
+#Displays page for saving the new body
+#
 get('/user/bodies') do
     slim(:"/bodies/create")
 end
 
+#Saves the new body
+#
 post('/user/bodies') do
     bodyname = params[:bodyname]
     save_body(bodyname)
     redirect('/user/bodies/index')
 end
 
+#Displays page for showing all your bodies
+#
 get('/user/bodies/index') do
     result = all_bodies_for_user()
     slim(:"/bodies/index",locals:{bodies:result, bodiesadmin:nil})
 end  
 
+#Displays page for showing everyones bodies
+#
 get('/admin/bodies/index') do
     resultadmin = all_bodies_for_everyone()
     slim(:"/bodies/index",locals:{bodiesadmin:resultadmin})
 end  
 
+#Displays page for editing body
+#
 get('/user/bodies/:id/edit') do
     id = params[:id].to_i
     edit_body(id)
 end
 
+#Updates body
+#
 post('/user/bodies/:id/update') do
     if "admin"==session[:authority]
         new_user_id = params[:user_id].to_i
@@ -91,20 +113,48 @@ post('/user/bodies/:id/update') do
     redirect('/user/bodies/index')
 end
 
+#Deletes body
+#
 post('/user/bodies/:id/delete') do
     body_id = params[:id].to_i
     delete_body(body_id)
     redirect('/user/bodies/index')
 end
 
+#Displays page for login
+#
 get('/showlogin') do
     slim(:login)
 end  
 
+#Logs in user
+#
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    if !field_empty?(username, password)
+        if user_exist?(username) && passwordvalidation(username, password)
+                login(username)
+                flash[:notice] = "Inloggningen lyckades!"
+                redirect('/user/bodies/new')
+        else
+            flash[:notice] = "Användaren finns inte eller så stämmer inte Lösenordet!"
+            redirect('/showlogin')
+        end
+    else
+        flash[:notice] = "Ett av fälten lämnades tomt!"
+        redirect('/showlogin')
+    end
+end
+
+#Displays page for register
+#
 get('/register') do
     slim(:register)
 end
 
+#Registers user
+#
 post('/register') do
     username = params[:username]
     password = params[:password]
@@ -134,24 +184,8 @@ post('/register') do
     end
 end
 
-post('/login') do
-    username = params[:username]
-    password = params[:password]
-    if !field_empty?(username, password)
-        if user_exist?(username) && passwordvalidation(username, password)
-                login(username)
-                flash[:notice] = "Inloggningen lyckades!"
-                redirect('/user/bodies/new')
-        else
-            flash[:notice] = "Användaren finns inte eller så stämmer inte Lösenordet!"
-            redirect('/showlogin')
-        end
-    else
-        flash[:notice] = "Ett av fälten lämnades tomt!"
-        redirect('/showlogin')
-    end
-end
-
+#Logouts user
+#
 get('/logout') do
     session.clear
     redirect('/showlogin')
